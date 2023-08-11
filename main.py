e9f51fd760638cd98a2ebff6d69287c10e76560d
Nathan Stanley <nstanley@bandwidth.com>
2023-08-11 12:03:08 -0400

    training jupyter notebook

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from fastai.data.block import CategoryBlock, DataBlock, get_image_files
-from fastai.vision.augment import ImageBlock
+from fastai.vision.augment import (
+    CategoryBlock,
+    DataBlock,
+    ImageBlock,
+    RandomSplitter,
+    Resize,
+    get_image_files,
+)
+from fastai.vision.learner import Learner
+from fastai.vision.models import resnet18
 
 
-def d4_func(fname):
-    print(fname.name[:2])
-
-    return "cat" if fname.name[0].isupper() else "dog"
+def label_func(fname):
+    return fname.name.split("_")[0]
 
 
-def main():
-    dblock = DataBlock(
-        blocks=(ImageBlock, CategoryBlock), get_items=get_image_files, get_y=d4_func
-    )
-    dsets = dblock.datasets("./training-data/train")
-    dsets.train[0]
-
-
-if __name__ == "__main__":
-    main()
+dblock = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),
+    get_items=get_image_files,
+    get_y=label_func,
+    splitter=RandomSplitter(),
+    item_tfms=Resize(300),
+)
+dls = dblock.dataloaders("./training-data/train")
+learn = Learner(
+    dls,
+    resnet18,
+)
+learn.fit(5)
+learn.export("foo.pkl")
